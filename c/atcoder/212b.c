724861a6567c583060cced3f580aa27060105397
Ogawa <3223mq118@gmail.com>
2025-05-25 17:41:09 +0900

    a

--- c/atcoder/212b.c
+++ c/atcoder/212b.c
@@ -4,18 +4,41 @@ int main(void) {
     int array[4];
     int i = 0;
     int weakFlag = 0;
+    char tempStr[5];
+
+    scanf("%s", tempStr);
     for(i = 0; i < 4; i++) {
-        scanf("%d", &array[i]);
+        array[i] = tempStr[i] - '0';
     }
+    
+    
     for(i = 0; i < 3; i++) {
         if(array[i] == array[i + 1]) {
             weakFlag = 1;
             continue;
         }
         weakFlag = 0;
+        break;
     }
     if(weakFlag == 1) {
         printf("Weak");
+        return 0;
     }
-    printf("outside");
+
+    for(i = 0; i < 3; i++) {
+        if(array[i+1] == array[i] + 1) {
+            weakFlag = 1;
+            continue;
+        } else if((array[i] == 9) && (array[i+1] == 0)) {
+            weakFlag = 1;
+            continue;
+        }
+        weakFlag = 0;
+        break;
+    }
+    if(weakFlag == 1) {
+        printf("Weak");
+    } else {
+        printf("Strong");
+    }    
 }
--- c/atcoder/test.c
+++ c/atcoder/test.c
@@ -1,48 +1,5 @@
 #include <stdio.h>
 
 int main(void) {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int n = 0;
-    int l = 0;
-    int countTilda = 0;
-    int baseArray[300000] = {0};
-    int tempArray[300000] = {0};
-
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++) {
-        scanf("%d", &baseArray[i]);
-    }
-
-    for(i = 0; i < n; i++) {
-        for(j = 4; j <= n - i; j++) {
-            int countSmall = 0;
-            int countBig = 0;
-
-            for(k = 0; k < j; k++) {
-                tempArray[k] = baseArray[i + k];
-            }
-
-            if (tempArray[0] >= tempArray[1]) {
-                continue;  // 条件1: A1 < A2 を満たさない
-            }
-
-            for(l = 1; l < j - 1; l++) {
-                if((tempArray[l] < tempArray[l-1]) && (tempArray[l] < tempArray[l+1])) {
-                    countSmall++;
-                } else if ((tempArray[l] > tempArray[l-1]) && (tempArray[l] > tempArray[l+1])) {
-                    countBig++;
-                }
-            }
-
-            if((countSmall == 1) && (countBig == 1)) {
-                countTilda++;
-            }
-        }
-    }
-
-    printf("%d\n", countTilda);
-    return 0;
+    
 }
